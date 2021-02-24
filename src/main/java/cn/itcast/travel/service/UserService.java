d50db9f28cea27dba77f3d4062e51abb1e6bbc94
xiazehua <1101779373@qq.com>
2021-02-24 18:16:27 +0800

    User接口创建，和实现类

---
+++ src/main/java/cn/itcast/travel/service/UserService.java
@@ -0,0 +1,11 @@
+package cn.itcast.travel.service;
+
+/**
+ * Software：IntelliJ IDEA 2020.1 x64
+ * CassName:      UserService
+ * Author:         夏泽华
+ * Date:           2021/2/24 18:14
+ * 接口描述：
+ */
+public interface UserService {
+}
---
+++ src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.service.UserService;
+
+/**
+ * Software：IntelliJ IDEA 2020.1 x64
+ * CassName:      UserServiceImpl
+ * Author:         夏泽华
+ * Date:           2021/2/24 18:14
+ * 描述：
+ */
+public class UserServiceImpl implements UserService {
+}
